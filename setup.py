2207ce38ef464f810b39aa6a3937c2cf3c564191
helgi <helgith@gmail.com>
2013-03-29 21:03:01 +0000

    Restructure the setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from os.path import join, dirname
 
-PKG='Command'
-VERSION='0.1.0'
 
-setup(name=PKG,
-      version=VERSION,
-      description="Command runner with debug capabilities",
-      author="Helgi Þorbjörnsson",
-      author_email="helgi@php.net",
-      url="http://github.com/helgi/python-command",
-      packages = find_packages(),
-      install_requires = [],
-      license = "MIT License",
-      keywords="subprocess,command",
-      zip_safe = True,
-      test_suite="tests",
-      tests_require=['coverage', 'mock'])
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
+PKG = 'Command'
+VERSION = '0.1.0'
+
+setup(
+    name=PKG,
+    version=VERSION,
+    description="Command runner with debug capabilities",
+    long_description=read('README.md'),
+    author="Helgi Þorbjörnsson",
+    author_email="helgi@php.net",
+    url="http://github.com/helgi/python-command",
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT License",
+    keywords="subprocess,command",
+    zip_safe=True,
+    test_suite="tests",
+    tests_require=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
